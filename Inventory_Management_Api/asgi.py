6a7376cf94dcf0070b012eb421f55e6909ee9376
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-15 19:45:12 +0200

    Update asgi.py

--- Inventory_Management_Api/asgi.py
+++ Inventory_Management_Api/asgi.py
@@ -1,1 +1,6 @@
+import os
+from django.core.asgi import get_asgi_application
 
+os.environ.setdefault('Django_Settings_Module', 'inventory_management_system.settings')
+
+application = get_asgi_application()
